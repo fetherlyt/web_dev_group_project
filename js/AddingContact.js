bc8b0404b5903fe6edfc625974e01e151b5d8ed1
wgmoo <wmoore@edgewood.edu>
2018-12-03 19:51:49 -0600

    created adding contact page
    added function to work off listener of contact add button
    --will need to update with correct ID of button when
    --supplied by andrew

---
+++ js/AddingContact.js
@@ -0,0 +1,3 @@
+function handleContactAdd(event){
+
+}
---
+++ js/common.js
@@ -0,0 +1,7 @@
+$(function() {
+    //update with correct button ID for adding a new contact
+    $("#contactUpdateId").click(function(event){
+        handleContactAdd(event);
+    });
+
+});
