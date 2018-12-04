f1a8ab010303e8f6ee138ad2a175c8643f657548
wgmoo <wmoore@edgewood.edu>
2018-12-03 20:21:39 -0600

    continuing to build page and added notes

--- js/AddingContact.js
+++ js/AddingContact.js
@@ -18,4 +18,52 @@ function displayContactEntryForm() {
         [{"name":"style","value":"padding: 1em"}]);
     content.append(contactAddContainer);
 
+    var table = createNode("table", []);
+    contactAddContainer.append(table);
+    var thead = createNode("th", []);
+    contactAddContainer.append(thead);
+
+    var tr = createNode("tr", []);
+    thead.append(tr);
+    var td = createNode("td",
+        [{"name":"class", "value":"label"}]);
+    td.innerHTML = "Department: ";
+    tr.append(td);
+
+    td = createNode("td", []);
+    tr.append(td);
+    var departmentName = createNode("span", []);
+    //add in the portion that calls the department name
+    departmentName.innerHTML = "";
+    td.append(departmentName);
+
+    tr = createNode("tr", []);
+    thead.append(tr);
+    td = createNode("td",
+        [{"name":"class", "value":"label"}]);
+    td.innerHTML = "First Name: ";
+    tr.append(td);
+
+    td = createNode("td", []);
+    tr.append(td);
+    var firstNameTxt = createNode("input",
+        [{"name":"type","value":"text"},
+            {"name":"id","value":"firstNameTxtId"},
+            {"name":"name","value":"firstName"},
+            {"name":"value","value":""}]);
+    td.append(firstNameTxt);
+    var fdRequired = createNode("span",
+        [{"name":"class","value":"error"}]);
+    fdRequired.innerHTML = "*";
+    td.append(fdRequired);
+
+    //Middle Initial
+    //Last name *required*
+    //Primary checkbox
+    //phone *required*
+    //Email *required*
+    //title *required*
+
+    //save and cancel buttons
+    //text of "* Required" under buttons
 }
